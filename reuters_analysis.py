256f82fc2c0e14128ae2b7aea4d1d294e60529e8
Pinto Spindler, Stephan <25225092+s-spindler@users.noreply.github.com>
2024-08-09 15:28:41 +0200

    Add analysis cases and move around cells

--- reuters_analysis.py
+++ reuters_analysis.py
@@ -6,63 +6,163 @@ app = marimo.App(width="medium")
 
 @app.cell
 def __(mo):
-    mo.md(r"""Result summary by Reuters: thttps://reutersinstitute.politics.ox.ac.uk/digital-news-report/2024/germany""")
+    mo.md(
+        r"""
+        United Internet Media post: https://www.united-internet-media.de/at/newsroom/vermarkterblog/blog/show/digital-news-report-2024-webde-und-gmx-unter-den-top-ten-der-meistgenutzten-online-news-sites/
+
+        Result summary by Reuters: https://reutersinstitute.politics.ox.ac.uk/digital-news-report/2024/germany
+        """
+    )
     return
+
+
+@app.cell
+def __(alt, mo, trust_long_df):
+    mo.ui.altair_chart(
+        alt.Chart(trust_long_df)
+        .mark_circle()
+        .encode(x="rating", y="media", size="count(rating):Q")
+    )
+    return
+
+
+@app.cell
+def __(alt, mo, trust_long_df):
+    mo.ui.altair_chart(
+        alt.Chart(trust_long_df)
+        .mark_boxplot()
+        .encode(alt.X("rating"), alt.Y("media"))
+    )
+    return
+
+
+@app.cell
+def __(alt, mo, trust_long_df):
+    mo.ui.altair_chart(
+        alt.Chart(
+            trust_long_df.groupby(["media", "rating"])["rating"]
+            .count()
+            .reset_index(name="count")
+        )
+        .mark_line()
+        .encode(x="rating", y="count", color="media")
+    )
+    return
+
+
+@app.cell
+def __(reuters_grouped_df):
+    reuters_grouped_df.div(reuters_grouped_df.sum(axis=1), axis=0).style.format(
+        {c: "{:,.2%}" for c in reuters_grouped_df.columns.values}
+    ).background_gradient(axis=0)
+    return
+
+
+@app.cell
+def __(trust_pivot_df):
+    # From Reuters page: "6–10 coded as ‘Trust’, 5 coded as ‘Neither’, 0–4 coded as ‘Don’t trust’"
+    reuters_grouped_df = trust_pivot_df.assign(
+        trust=trust_pivot_df[list(range(6, 11))].sum(axis=1),
+        neither=trust_pivot_df[5],
+        no_trust=trust_pivot_df[list(range(0, 5))].sum(axis=1),
+    ).drop(list(range(0, 11)), axis=1)
+    reuters_grouped_df
+    return reuters_grouped_df,
+
+
+@app.cell
+def __(trust_pivot_df):
+    def calc_nps(row):
+        cols = list(range(0, 11))
+        detractor_cols = list(range(0, 7))
+        promoter_cols = [9, 10]
+
+        total = row[cols].sum()
+        detractors = row[detractor_cols].sum()
+        promoters = row[promoter_cols].sum()
+
+        nps = promoters / total * 100 - detractors / total * 100
+
+        return nps
+
+
+    trust_pivot_df.apply(calc_nps, axis=1)
+    return calc_nps,
+
+
+@app.cell
+def __(trust_long_df):
+    trust_pivot_df = (
+        trust_long_df.groupby(["media", "rating"])["rating"]
+        .count()
+        .reset_index(name="count")
+        .pivot_table(index="media", columns="rating", values="count")
+    )
+    trust_pivot_df
+    return trust_pivot_df,
 
 
 @app.cell
 def __(trust_df):
-    trust_df
-    return
-
-
-@app.cell
-def __(column_name_to_media, relevant_columns, reuters_df):
-    trust_df = reuters_df.filter(relevant_columns).rename(columns=column_name_to_media)
-    trust_df
-    return trust_df,
-
-
-@app.cell
-def __(number_to_col_name, number_to_media):
-    column_name_to_media = {number_to_col_name[num]: number_to_media[num] for num in number_to_col_name}
-    return column_name_to_media,
-
-
-@app.cell
-def __(relevant_columns):
-    number_to_col_name = {col[col.rindex("_") + 1:]: col for col in relevant_columns}
-    return number_to_col_name,
-
-
-@app.cell
-def __():
-    number_to_media = {
-        "46": "ARD Tagesschau",
-        "47": "ZDF Heute",
-        "48": "RTL aktuell",
-        "50": "n-tv",
-        "51": "Der Spiegel",
-        "52": "Die ZEIT",
-        "53": "Stern",
-        "54": "Focus",
-        "55": "Süddeutsche Zeitung",
-        "56": "Bild",
-        "57": "FAZ",
-        "58": "Regional or local newspaper",
-        "60": "t-online",
-        "889": "WELT",
-        "890": "webde"
-    }
-    return number_to_media,
+    trust_long_df = trust_df.melt(var_name="media", value_name="rating")
+    trust_long_df
+    return trust_long_df,
 
 
 @app.cell
 def __(reuters_df):
-    all_columns = reuters_df.columns.values
-    import re
-    relevant_columns = [col for col in all_columns if re.search("Q6_2018_trust_rb_\d{2,3}", col)]
-    return all_columns, re, relevant_columns
+    def get_relevant_columns(all_columns):
+        import re
+
+        return [
+            col
+            for col in all_columns
+            if re.search("Q6_2018_trust_rb_\d{2,3}", col)
+        ]
+
+
+    def map_column_name_to_media(relevant_columns):
+        number_to_col_name = {
+            col[col.rindex("_") + 1 :]: col for col in relevant_columns
+        }
+
+        number_to_media = {
+            "46": "ARD Tagesschau",
+            "47": "ZDF Heute",
+            "48": "RTL aktuell",
+            "50": "n-tv",
+            "51": "Der Spiegel",
+            "52": "Die ZEIT",
+            "53": "Stern",
+            "54": "Focus",
+            "55": "Süddeutsche Zeitung",
+            "56": "Bild",
+            "57": "FAZ",
+            "58": "Regional or local newspaper",
+            "60": "t-online",
+            "889": "WELT",
+            "890": "webde",
+        }
+
+        return {
+            number_to_col_name[num]: number_to_media[num]
+            for num in number_to_col_name
+        }
+
+
+    _relevant_columns = get_relevant_columns(reuters_df.columns.values)
+    column_name_to_media = map_column_name_to_media(_relevant_columns)
+
+    trust_df = reuters_df.filter(_relevant_columns).rename(
+        columns=column_name_to_media
+    )
+    trust_df
+    return (
+        column_name_to_media,
+        get_relevant_columns,
+        map_column_name_to_media,
+        trust_df,
+    )
 
 
 @app.cell
@@ -73,14 +173,10 @@ def __(pandas):
 
 @app.cell
 def __():
-    import pandas
-    return pandas,
-
-
-@app.cell
-def __():
     import marimo as mo
-    return mo,
+    import altair as alt
+    import pandas
+    return alt, mo, pandas
 
 
 if __name__ == "__main__":
